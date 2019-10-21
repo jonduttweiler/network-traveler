0d5cdadf3159adfe3787dfec1619ca29756ebc56
Jonatan Edgardo Duttweiler <jduttweiler@mpa.santafe.gov.ar>
2019-10-21 07:46:34 -0300

    the input and output path is no longer necessary

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: "development",
-    entry: "./src/app.js",
     output: {
-        path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
     plugins: [
