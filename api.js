ba98af85d314e1a80a7a45d8ede1982eac7465ac
Jonatan Edgardo Duttweiler <jduttweiler@mpa.santafe.gov.ar>
2019-10-21 13:23:14 -0300

    refact

--- api.js
+++ api.js
@@ -19,25 +19,34 @@ app.use((req, res, next) => {
 });
 
 
-//TODO: CONSIDERAR CONCURRENCIA Y STATELESS
-//las imagenes descargadas con el download imgs, tienen que almacenarse en un directorio temporal
+const src_dir =  path.join(".","tmp","src")
+const dist_dir = path.join(".","tmp","dist") 
+const zips_dir = path.join(".","tmp","zips") 
+
+
 app.post('/bundle', async (req, res, next) => {
   try {
 
     let id = new Date().getTime();
 
     let network = req.body.network;
-    let travel = req.body.travel; 
+    let travel = req.body.travel;
+    await make_src_dir(src_dir, id, network, travel);
 
-    let src_dir = await make_src_dir(id,network,travel);
-    await bundle_it(src_dir,`dists/${id}`); 
-    await zip_folder(`dists/${id}`,`zips/${id}`);
-  
-    res.sendFile(path.resolve(`./zips/${id}.zip`)); //TODO: remove generated files
+    let src = path.join(src_dir,`${id}`);
+    let dist= path.join(dist_dir,`${id}`);
+    let zips= path.join(zips_dir,`${id}`);
 
+    await bundle_it(src, dist);
+    await zip_folder(dist, zips);
 
-
-    return
+    console.log("sending")
+    res.sendFile(path.resolve(`${zips}.zip`), err => {
+      if (!err) {
+        remove_generated_files(id);
+      }
+    });
+    return;
   } catch (err) {
     console.log(err);
     res.status(500).json({ err });
@@ -45,47 +54,61 @@ app.post('/bundle', async (req, res, next) => {
 
 })
 
-make_src_dir = async (id,network,travel) =>{
-    let src_dir = path.join("srcs", `${id}`);
-    let src_img = path.join(src_dir,"imgs");
-    let src_data = path.join(src_dir,"data");
-    let dist_dir ="./img";
+const make_src_dir = async (src_dir, id, network, travel) => {
+  let src = path.join(src_dir, `${id}`);
+  let src_img = path.join(src, "imgs");
+  let src_data = path.join(src, "data");
+  let dist_dir = "./img";
 
-    await copy_folder("./assets",src_dir);
-  
-    fs.mkdirSync(src_img);
-    fs.mkdirSync(src_data);
+  await copy_folder("./assets", src);
 
-    await download_files(network,src_img,dist_dir);
-    writejson(network,`${src_data}/network.json`);
-    writejson(travel,`${src_data}/travel.json`);
+  fs.mkdirSync(src_img);
+  fs.mkdirSync(src_data);
 
-    return src_dir;
+  await download_files(network, src_img, dist_dir);
+  writejson(network, `${src_data}/network.json`);
+  writejson(travel, `${src_data}/travel.json`);
+
+  return ;
 }
 
 
-bundle_it = (src_dir,output_dir) => {
-  return execute("/home/jduttweiler/network-traveler/node_modules/.bin/webpack", 
-                 [
-                  '--config', `webpack.config.js`,
-                  '--entry', `./${src_dir}/app.js`,
-                  '--output-path', `${output_dir}`
-                ]
-                 
-                 
+const bundle_it = (src_dir, output_dir) => {
+  return execute("/home/jduttweiler/network-traveler/node_modules/.bin/webpack",
+    [
+      '--config', `webpack.config.js`,
+      '--entry', `./${src_dir}/app.js`,
+      '--output-path', `${output_dir}`
+    ]
+
+
   );
 }
 
-zip_folder = (src,output) => {
-  return execute("zip",['-r',`${output}`,`${src}`]);
+const zip_folder = (src, output) => {
+  return execute("zip", ['-r', `${output}`, `${src}`]);
 }
 
-copy_folder = (src, dest) => {
-  return execute('cp', ['-r', src,dest]);
+const copy_folder = (src, dest) => {
+  return execute('cp', ['-r', src, dest]);
+}
+
+const remove_generated_files = id => {
+  rm(`tmp/src/${id}`);
+  rm(`tmp/dist/${id}`);
+  rm(`tmp/zips/${id}.zip`);
 }
 
 
-execute = (command, args) => {
+const rm = (dir) => {
+  if (dir.startsWith("tmp")) {
+    return execute('rm', ['-r', dir]);
+  }
+}
+
+
+
+const execute = (command, args) => {
   return new Promise((resolve, reject) => {
     let stdout = "";
     let stderr = "";
@@ -105,8 +128,8 @@ execute = (command, args) => {
 }
 
 
-function writejson(network,pathtofile){
-  fs.writeFileSync(pathtofile,JSON.stringify(network,null,3));
+function writejson(network, pathtofile) {
+  fs.writeFileSync(pathtofile, JSON.stringify(network, null, 3));
 }
 
 
