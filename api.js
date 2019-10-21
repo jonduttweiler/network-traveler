7d1d9c20cd3c3c1cca977bf671a11bbb286ae580
Jonatan Edgardo Duttweiler <jduttweiler@mpa.santafe.gov.ar>
2019-10-21 07:38:39 -0300

    download resources and bundle with webpack

--- api.js
+++ api.js
@@ -24,19 +24,24 @@ app.use((req, res, next) => {
 app.post('/bundle', async (req, res, next) => {
   try {
 
-    let network = req.body.network;
-    let travel = req.body.travel; //copy travel to data?
+    let id = new Date().getTime();
 
-    await download_files(network);
-    await npm_build(); 
-    let generated_zip = path.resolve("zips",`dist-${new Date().getTime()}.zip`);
+    let network = req.body.network;
+    let travel = req.body.travel; 
+
+    let src_dir = await make_src_dir(id,network,travel);
+    await build_it(src_dir); 
+
+    
+    /*let generated_zip = path.resolve("zips",`dist-${id}.zip`);
 
     await zip_folder("dist",generated_zip);
     
 
-    //remove temp files async?
+    //remove temp files async?*/
     console.log("done!");
-    return res.sendFile(generated_zip); //TODO: DELETE GENERATED ZIP
+    //return res.sendFile(generated_zip); //TODO: DELETE GENERATED ZIP
+    res.json({});
   } catch (err) {
     console.log(err);
     res.status(500).json({ err });
@@ -44,10 +49,47 @@ app.post('/bundle', async (req, res, next) => {
 
 })
 
-//that returns a promise
-npm_build = _ => {
-  return execute('npm', ['run', 'build']);
+make_src_dir = async (id,network,travel) =>{
+    let src_dir = path.join("srcs", `${id}`);
+    let src_img = path.join(src_dir,"imgs");
+    let src_data = path.join(src_dir,"data");
+    let dist_dir ="./img";
+
+    await copy_folder("./assets",src_dir);
+  
+    fs.mkdirSync(src_img);
+    fs.mkdirSync(src_data);
+
+    await download_files(network,src_img,dist_dir);
+    writejson(network,`${src_data}/network.json`);
+    writejson(travel,`${src_data}/travel.json`);
+
+    return src_dir;
 }
+
+
+
+
+//that returns a promise
+build_it = src_dir => {
+  
+  console.log(src_dir);
+  //pasar parametros de source e ouput dir
+  return execute("/home/jduttweiler/network-traveler/node_modules/.bin/webpack", 
+                 [
+                  '--config', `webpack.config.js`,
+                  '--entry', `./${src_dir}/app.js`,
+                  '--output-path', `dists/test/`
+                ]
+                 
+                 
+                );
+}
+
+copy_folder = (src, dest) => {
+  return execute('cp', ['-r', src,dest]);
+}
+
 
 zip_folder = (src, dest) => {
   return execute('zip', ['-r', dest, src]);
@@ -70,6 +112,11 @@ execute = (command, args) => {
     });
 
   });
+}
+
+
+function writejson(network,pathtofile){
+  fs.writeFileSync(pathtofile,JSON.stringify(network,null,3));
 }
 
 
