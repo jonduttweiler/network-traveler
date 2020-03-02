fe8dae46acabd64086abf7f4dc1fbacbd79cab0b
Jonatan Edgardo Duttweiler <jduttweiler@mpa.santafe.gov.ar>
2020-03-02 11:14:50 -0300

    new format of steps, positions & scale instead of nodeid

--- assets/travel.js
+++ assets/travel.js
@@ -1,25 +1,18 @@
 //handle click on buttons
 const travel = require('./data/travel.json');
 
-if(travel && travel.length){ //solo si tenemos un travel que seguir ¯\_(ツ)_/¯
-    let current_idx = -1;
-    document.getElementById('travel-steps').innerHTML = `[${travel.join(", ")}]`;
-    
+if (travel && travel.length) { //solo si tenemos un travel que seguir ¯\_(ツ)_/¯
+    let current_idx = -1; //create a class!
+    document.getElementById('travel-steps').innerHTML = `[${travel.map(step => `[${step.x}-${step.y}-${step.scale}]`).join(", ")}]`;
+
     const show_current_idx = _ => {
-        document.getElementById('current-step').innerHTML = current_idx >= 0 ? `Current: ${travel[current_idx]}` : "";
+        document.getElementById('current-step').innerHTML = current_idx >= 0 ? `Current: ${current_idx}` : "";
     }
-    
+
     const show_current_in_network = current_idx => {
-        let current_scale = current_idx === 0 ? 1 : 2;
-    
-        network.focus(travel[current_idx], {
-            scale: current_scale,
-            animation: true
-        });
-        network.setSelection({ nodes: [travel[current_idx]] }, { highlightEdges: false })
-    
+        network.moveTo({ ...travel[current_idx], animation: true });
     }
-    
+
     const prev = _ => {
         current_idx = (current_idx == 0) ? (travel.length - 1) : (current_idx - 1) % travel.length;
         show_current_in_network(current_idx);
@@ -30,11 +23,11 @@ if(travel && travel.length){ //solo si tenemos un travel que seguir ¯\_(ツ)_/�
         show_current_in_network(current_idx);
         show_current_idx();
     }
-    
+
 
     document.getElementById('prev-btn').addEventListener("click", prev);
     document.getElementById('next-btn').addEventListener("click", next);
 } else {
-    document.getElementById('prev-btn').style.display="none";
-    document.getElementById('next-btn').style.display="none";
+    document.getElementById('prev-btn').style.display = "none";
+    document.getElementById('next-btn').style.display = "none";
 }
