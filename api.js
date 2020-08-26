033ac8c621347b06835971aa850cf9b6ff10c387
Emanuel Banchio <ebanchio@mpa.santafe.gov.ar>
2020-08-26 11:42:13 -0300

    Se extrae conf de puerto. Endpoint para consultar versión de la app.

--- api.js
+++ api.js
@@ -1,8 +1,9 @@
-const APP_PORT = 3000;
+const APP_PORT = process.env.APP_PORT || 8080;
 
-const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 const bundlerRouter = require("./routes/bundler");
-
 
 const app = express();
 app.use(express.json({limit: '50mb'}));
@@ -17,6 +18,25 @@ app.use((req, res, next) => {
 
 app.use(bundlerRouter);
 
+let versionCache;
+app.get("/", async (req, res, next) => {
+    try {
+        let version = versionCache;
+        let name;
+        if (!version) {
+            const pckPath = path.resolve(__dirname, "package.json");
+            const pkgraw = await fs.promises.readFile(pckPath);
+            const pkg = JSON.parse(pkgraw);
+            name = pkg.name;
+            version = pkg.version;
+            versionCache = pkg.version;
+        }
+        return res.json({ app:name, version });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: err })
+    }
+});
 
 app.listen(APP_PORT, _ => {
   console.log("INFO", "BOOTSTRAP", `App listening on port ${APP_PORT}!`);
